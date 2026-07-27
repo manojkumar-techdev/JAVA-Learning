54f1d33c7ac2de89275d0e5c76775f729b47c3ce
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-28 00:00:51 +0200

    Update 01_basics.java

--- 01_basics.java
+++ 01_basics.java
@@ -5,6 +5,15 @@ public class Learning {
    public static void main(String[] args) {
        System.out.println("Hello World");
    }
+}
+
+//# -- 1
+// hello world
+
+public class Learning {
+   public static void main(String[] args) {
+       System.out.println("Hello World");
+   }
    //# -- 1
 // hello world
 
