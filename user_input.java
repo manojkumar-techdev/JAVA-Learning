d1788d8ac4e362f64ec6077610dd8fc6d8f1bdb3
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-30 19:28:32 +0000

    user_input

---
+++ user_input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        System.out.println("You entered " + number1);
+    }
+}
