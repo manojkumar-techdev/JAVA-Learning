81297b5754a53d9c831e76386530651bf50f7624
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-31 23:38:13 +0200

    Update array.java

--- array.java
+++ array.java
@@ -18,3 +18,21 @@
    // clears that array will contain 5 elements
    int[] numbers = new int[5];
 }
+
+
+// adding elements to array
+
+{
+   // adding elements to array
+   int[] numbers = new int[5];
+
+   numbers[0] = 10;
+   numbers[1] = 20;
+   numbers[2] = 30;
+   numbers[3] = 40;
+   numbers[4] = 50;
+
+
+   System.out.println(numbers[0]);
+   System.out.println(numbers[1]);
+}
