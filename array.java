9b90dc0ae976ead05177145e0ca32511b4e406aa
Manoj Kumar <contact@manojkumar-dev.com>
2026-07-31 21:36:58 +0000

    array

---
+++ array.java
@@ -0,0 +1,11 @@
+// declare an array
+
+{
+   // array in java
+   // stores multiple values of same data type
+   // in a single variable
+
+   // declare an array of numbers
+   // method 1 - recommended one
+   int[] numbers;
+}
