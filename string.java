8ebb53af1e3f14596bf69eabb751a124fa0a3212
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-07 20:26:39 +0000

    string in java

---
+++ string.java
@@ -0,0 +1,8 @@
+// string literals are characters stored in double quotes
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        System.out.println(str)
+    }
+}
